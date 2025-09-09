3558973e50a2cd80dc20be0122033f0990603e72
13priyankameena <13priyankameena13@gmail.com>
2025-09-09 10:46:08 +0530

    deleted unnecessary file

--- src/Component/Backend/Routes/chartRoute.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { createChartData, fetchChartData } from "../controller/chartController.js";
-
-const router = express.Router();
-
-router.post("/create", createChartData);
-router.get("/fetch", fetchChartData);
-
-export default router;
